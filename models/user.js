7caddf0634c1de8ff110c44b4a725da655dcd8fe
Vikram <singh.vikramsingh06@gmail.com>
2024-07-25 13:24:11 -0400

    backend

---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  fatherName: { type: String, required: true },
+  country: { type: String, required: true },
+  dob: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: false },
+  id: { type: String, unique: true },
+});
+
+export default mongoose.model("User", userSchema);
